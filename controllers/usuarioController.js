2a3b95ce276e570cbc26cd4befc33dde4d91d833
EslebanPerez <eslebanperez@gmail.com>
2022-08-26 04:35:31 -0500

    Añadiendo más rutas

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,7 +1,15 @@
 const formularioLogin = ( req, res )=>{
     res.render('auth/login',{
-        autenticado: false
+        
+    });
+}
+const formularioRegistro = ( req, res )=>{
+    res.render('auth/registro',{
+         
     });
 }
 
-export { formularioLogin }
+export { 
+    formularioLogin, 
+    formularioRegistro 
+}
