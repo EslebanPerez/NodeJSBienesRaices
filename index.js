46e52a46b99db69e4e282491c89896007d91b469
EslebanPerez <eslebanperez@gmail.com>
2022-08-26 03:46:35 -0500

    Habilitando Template engine

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ import usuarioRoutes from "./routes/usuarioRoutes.js"
 const app = express();
 const port = 3000;
 
-app.use('/', usuarioRoutes);
+//Habilitando Pug 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+//Routing
+app.use('/auth', usuarioRoutes);
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
