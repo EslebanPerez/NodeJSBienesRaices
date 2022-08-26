d5942e4a5385eee8b2806fcb94413fb1732785fa
EslebanPerez <eslebanperez@gmail.com>
2022-08-26 17:09:12 -0500

    Mostrar nombre dinámicamente a las páginas

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,11 +1,11 @@
 const formularioLogin = ( req, res )=>{
     res.render('auth/login',{
-        
+        title : "Ingresar"
     });
 }
 const formularioRegistro = ( req, res )=>{
     res.render('auth/registro',{
-         
+        title : "Crear cuenta"
     });
 }
 
