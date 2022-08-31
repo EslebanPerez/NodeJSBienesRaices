d1d4e40afca94f4d9e778cae305aeec2bd9cee14
EslebanPerez <eslebanperez@gmail.com>
2022-08-31 00:35:39 -0500

    Agregando conexión a bases de datos con variables de entorno

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js"
+import db from "./config/db.js"
 
 const app = express();
 const port = 3000;
+
+//Conexión a la base de datos
+try{
+  await db.authenticate();
+  console.log("Conexion correcta a la base de datos");
+} catch(error){
+  console.log(error);
+}
 
 //Habilitando Pug 
 app.set("view engine", "pug");
