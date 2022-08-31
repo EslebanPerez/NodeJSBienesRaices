b40737248eb085421c7a90502d65b6f451c3654a
EslebanPerez <eslebanperez@gmail.com>
2022-08-31 00:46:13 -0500

    Agregando especificación para modelo Usuario

---
+++ models/Usuario.js
@@ -0,0 +1,20 @@
+import { DataTypes } from "sequelize";
+import db from '../config/db.js'
+
+const Usuario = db.define('usuarios',{
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token:  DataTypes.STRING,
+    confirmamdo: DataTypes.BOOLEAN
+})
+ export default Usuario;
