96faeaf359b9b8fb86c5c929373f1f1f783a92c2
EslebanPerez <eslebanperez@gmail.com>
2022-09-01 04:38:16 -0500

    Agregando validaciones en formulario con express-validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import {check, validationResult} from "express-validator"
 import Usuario from "../models/Usuario.js";
 
 const formularioLogin = ( req, res )=>{
@@ -11,6 +12,14 @@ const formularioRegistro = ( req, res )=>{
     });
 }
 const registrar = async ( req, res )=>{
+    await check ("username").notEmpty().withMessage('El nombre es obligatorio').run(req);
+    await check ("email").isEmail().withMessage('Eso no parece un email').run(req);
+    await check ("password").isLength({ min: 6 }).withMessage('La contraseña debe ser de al menos 6 caracteres').run(req);
+    await check ("repetirPassword").equals("password").withMessage('Las contraseñas no coinciden').run(req);
+    let resultado = validationResult(req);
+
+    res.json(resultado.array());
+
     const usuario = await Usuario.create(req.body);
     res.json(usuario);
 }
