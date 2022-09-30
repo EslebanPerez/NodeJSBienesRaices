08a09092dabdc4d9b4c5ef80a3abd5ecb1ce543c
EslebanPerez <eslebanperez@gmail.com>
2022-09-30 03:05:16 -0500

    Agregando relación de Propiedad con Usuario y Categoria

--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ import Usuario from './Usuario.js'
 
 // Precio.hasOne(Propiedad);
 Propiedad.belongsTo(Precio, {foreignKey: 'precioId'});
+Propiedad.belongsTo(Categoria, {foreignKey: 'categoriaId'});
+Propiedad.belongsTo(Usuario, {foreignKey: 'usuarioId'});
 
 export{
     Propiedad,
