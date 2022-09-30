e11c74193cd1d79b52d9070599a1b753b786adce
EslebanPerez <eslebanperez@gmail.com>
2022-09-30 02:53:01 -0500

    Agregando archivo para de hacer relaciones entre tablas de DB

---
+++ models/index.js
@@ -0,0 +1,14 @@
+import Propiedad from './Propiedad.js'
+import Precio from './Precio.js'
+import Categoria from './Categoria.js'
+import Usuario from './Usuario.js'
+
+// Precio.hasOne(Propiedad);
+Propiedad.belongsTo(Precio, {foreignKey: 'precioId'});
+
+export{
+    Propiedad,
+    Precio,
+    Categoria,
+    Usuario
+}
