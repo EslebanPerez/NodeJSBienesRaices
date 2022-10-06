a45e5b49d13fd49672728fed37781cdc62d9cc34
EslebanPerez <eslebanperez@gmail.com>
2022-10-06 03:18:16 -0500

    Leyendo datos de formulario y renombrando algunas variables para almacenar en la DB

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -1,7 +1,5 @@
 import { validationResult } from "express-validator";
-import Precio from "../models/Precio.js";
-import Categoria from "../models/Categoria.js";
-
+import {Precio, Categoria, Propiedad} from '../models/index.js'
 const admin = (req, res) => {
   res.render("propiedades/admin", {
     title: 'Mis propiedades',
@@ -47,6 +45,25 @@ const guardar = async(req, res) => {
     errores: resultado.array(),
     datos: req.body
   });
+  }
+  // Crear un registro
+  //console.log(req.body);
+  const {titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio:precioId, categoria:categoriaId} = req.body
+  try {
+    const propiedadGuardada = await Propiedad.create({
+      titulo,
+      descripcion,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+      precioId,
+      categoriaId
+    })
+  } catch (error) {
+    console.log(error);
   }
 }
 
