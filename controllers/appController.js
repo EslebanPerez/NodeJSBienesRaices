19cb7507287eae00865a52e5ce3adc9d008b293f
EslebanPerez <eslebanperez@gmail.com>
2023-01-17 23:12:16 -0600

    Filtrando y obteniendo registro de casas que se han creado recientemente

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,17 +2,27 @@ import { Precio, Categoria, Propiedad } from "../models/index.js"
 
 const inicio = async (req, res) => {
 
-  const [categorias, precios] = await Promise.all([
+  const [categorias, precios, casas] = await Promise.all([
     Categoria.findAll({raw: true}),
     Precio.findAll({raw: true}),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 1
+      },
+      include: [{
+        model: Precio,
+        as: 'precio'
+      }],
+      order: [['createdAt', 'DESC']]
+    })
   ])
-
-  console.log(categorias)
 
   res.render('inicio',{
     title: 'Inicio',
     categorias,
-    precios
+    precios,
+    casas
   })
 }
 
