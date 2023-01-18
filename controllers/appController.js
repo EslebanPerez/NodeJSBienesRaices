a78a0caa6e215872acb95437c1d5664ae7267d40
EslebanPerez <eslebanperez@gmail.com>
2023-01-17 23:58:42 -0600

    Mostrando registro de casa en la vista con estilos

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,13 +2,24 @@ import { Precio, Categoria, Propiedad } from "../models/index.js"
 
 const inicio = async (req, res) => {
 
-  const [categorias, precios, casas] = await Promise.all([
+  const [categorias, precios, casas, departamentos] = await Promise.all([
     Categoria.findAll({raw: true}),
     Precio.findAll({raw: true}),
     Propiedad.findAll({
       limit: 3,
       where: {
         categoriaId: 1
+      },
+      include: [{
+        model: Precio,
+        as: 'precio'
+      }],
+      order: [['createdAt', 'DESC']]
+    }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 2
       },
       include: [{
         model: Precio,
@@ -22,7 +33,8 @@ const inicio = async (req, res) => {
     title: 'Inicio',
     categorias,
     precios,
-    casas
+    casas,
+    departamentos
   })
 }
 
